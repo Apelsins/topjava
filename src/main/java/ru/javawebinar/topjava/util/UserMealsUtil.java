da725e02b867aa9972a98c9912201b13e8e76ebf
Apelsins <lightninglobe@gmail.com>
2017-07-22 19:37:52 +0300

    HW0 is completed

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -6,6 +6,7 @@ import ru.javawebinar.topjava.model.UserMealWithExceed;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Month;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -23,13 +24,26 @@ public class UserMealsUtil {
                 new UserMeal(LocalDateTime.of(2015, Month.MAY, 31,13,0), "Обед", 500),
                 new UserMeal(LocalDateTime.of(2015, Month.MAY, 31,20,0), "Ужин", 510)
         );
-        getFilteredWithExceeded(mealList, LocalTime.of(7, 0), LocalTime.of(12,0), 2000);
+        List<UserMealWithExceed> mealListExceed = getFilteredWithExceeded(mealList, LocalTime.of(7, 0), LocalTime.of(12,0), 2000);
+        for (UserMealWithExceed iter : mealListExceed) {
+            System.out.println(iter);
+        }
 //        .toLocalDate();
 //        .toLocalTime();
     }
 
     public static List<UserMealWithExceed>  getFilteredWithExceeded(List<UserMeal> mealList, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
-        // TODO return filtered list with correctly exceeded field
-        return null;
+        // create List UserMealWithExceed, wich will be returned
+        List<UserMealWithExceed> userMealWithExceedList = new ArrayList<>();
+        for (UserMeal iterUserMeal : mealList) {
+            if (TimeUtil.isBetween(iterUserMeal.getDateTime().toLocalTime(), startTime, endTime))
+                userMealWithExceedList.add(new UserMealWithExceed(
+                        iterUserMeal.getDateTime(),
+                        iterUserMeal.getDescription(),
+                        iterUserMeal.getCalories(),
+                        iterUserMeal.getCalories() > caloriesPerDay ? true : false // exceeded calories?
+                ));
+        }
+        return userMealWithExceedList;
     }
 }
